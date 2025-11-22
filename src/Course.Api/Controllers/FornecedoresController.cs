abd7703a3c48946b996fd7da6c5543f1c872f67c
Rafael Barbosa <rafha4061@gmail.com>
2025-11-22 11:16:05 -0300

    Definindo as Controllers

---
+++ src/Course.Api/Controllers/FornecedoresController.cs
@@ -0,0 +1,44 @@
+﻿using Course.Api.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Course.Api.Controllers
+{
+    [Route("api/fornecedores")]
+    public class FornecedoresController : MainController
+    {
+        public FornecedoresController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<FornecedorViewModel>> ObterTodos()
+        {
+
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<FornecedorViewModel>> ObterPorId(Guid id)
+        {
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<FornecedorViewModel>> Adicionar(FornecedorViewModel fornecedorViewModel)
+        {
+
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<FornecedorViewModel>> Atualizar(Guid id, FornecedorViewModel fornecedorViewModel)
+        {
+
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<FornecedorViewModel>> Excluir(Guid id)
+        {
+
+        }
+    }
+}
