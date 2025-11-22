eb8e81cc41b9d72b287b386a632d483bee24f28f
Rafael Barbosa <rafha4061@gmail.com>
2025-11-21 22:20:52 -0300

    Especializando repositórios das entidades

---
+++ src/Course.Data/Repository/FornecedorRepository.cs
@@ -0,0 +1,39 @@
+﻿using Course.Business.Interfaces;
+using Course.Business.Models;
+using Course.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Course.Data.Repository
+{
+    public class FornecedorRepository : Repository<Fornecedor>, IFornecedorRepository
+    {
+        public FornecedorRepository(MeuDbContext context) : base(context) { }
+
+        public async Task<Fornecedor> ObterFornecedorEndereco(Guid id)
+        {
+            return await Db.Fornecedores.AsNoTracking()
+                .Include(c => c.Endereco)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid id)
+        {
+            return await Db.Fornecedores.AsNoTracking()
+                .Include(c => c.Endereco)
+                .Include(C => C.Produtos)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId)
+        {
+            return await Db.Enderecos.AsNoTracking()
+                .FirstOrDefaultAsync(f => f.FornecedorId == fornecedorId);
+        }
+
+        public async Task RemoverEnderecoFornecedor(Endereco endereco)
+        {
+            Db.Enderecos.Remove(endereco);
+            await SaveChanges();
+        }
+    }
+}
